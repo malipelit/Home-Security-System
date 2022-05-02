4456c35a0728af67228024ddff9793be7e18feda
Mehmet Ali <malipelit59@gmail.com>
2022-05-02 18:54:48 +0300

    Comments added

--- code/main.py
+++ code/main.py
@@ -4,17 +4,20 @@ cam_front = cv2.VideoCapture(0)
 problem_signal = False
 
 while True:
-    ret, frame = cam_front.read()
+    # get the frame from the front camera
+    ret, front_frame = cam_front.read()
 
+    # show the frame
     if(ret):
         problem_signal = False
 
-        cv2.imshow("Camera Input", frame)
+        cv2.imshow("Camera Input", front_frame)
     else:
         if(not problem_signal):
-            print ("Frame Missing")
+            print ("Frame from the front CAM is missing")
             problem_signal = True
 
+    # to break the main loop press ESC
     key = cv2.waitKey(1)
     if key == 27:
         break
