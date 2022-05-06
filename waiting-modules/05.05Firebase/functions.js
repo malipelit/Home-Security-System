9950d121a5310fc2d62d85c6e692b19198f93e2c
barkingulez <96734188+barkingulez@users.noreply.github.com>
2022-05-06 13:15:30 +0300

    Delete waiting-modules/05.05Firebase directory

--- waiting-modules/05.05Firebase/functions.js
+++
@@ -1,7 +0,0 @@
-const userId = document.getElementById('userId');
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const age = document.getElementById('age');
-const addBtn = document.getElementById('addBtn');
-const updateBtn = document.getElementById('updateBtn');
-const removeBtn = document.getElementById('removeBtn'); 
